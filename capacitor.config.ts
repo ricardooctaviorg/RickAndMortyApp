070323f3ac9d526265a8815718772b3a5f5a18f6
Ricardo Octavio Rojas Garcia <ricardooctaviorg@gmail.com>
2021-08-10 21:10:28 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RickAndMortyApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
