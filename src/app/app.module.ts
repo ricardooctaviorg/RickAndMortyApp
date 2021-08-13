e09635b028637db44aed1d2b14d5e7e68c284b3f
Ricardo Octavio Rojas Garcia <ricardooctaviorg@gmail.com>
2021-08-13 11:33:56 -0500

    update to PWA dependecys and Firebse config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +16,12 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [BrowserModule
     , IonicModule.forRoot()
     , AppRoutingModule
-    , HttpClientModule],
+    , HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
